937bca57ef924594cde2ef8cae110e5aea62f6cd
xiaoxiaoweii <2679656468@qq.com>
2021-03-28 19:55:13 +0800

    setup() ref()函数

---
+++ vue3-1/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- eslint-disable -->
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h2>欢迎光临晓晓伟餐厅</h2>
+    <div>请选择服务员</div>
+    <div>
+      <button
+        v-for="(item, index) in girls"
+        :key="index"
+        @click="selectGirlFun(index)"
+      >
+        {{ index }}: {{ item }}
+      </button>
+      <div>你选择的服务员是: {{ selectGirl }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    const girls = ref(["服务员1", "服务员2", "服务员3"]);
+    const selectGirl = ref("");
+    const selectGirlFun = (index: number) => {
+      selectGirl.value = girls.value[index];
+    };
+    return {
+      girls,
+      selectGirl,
+      selectGirlFun
+    };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-1/src/main.ts
@@ -0,0 +1,5 @@
+// vue3独有
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
