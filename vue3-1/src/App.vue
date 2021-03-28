4ad9294144799e0d0a1f818e5175801a5cd03a24
xiaoxiaoweii <2679656468@qq.com>
2021-03-28 22:19:34 +0800

    watch

--- vue3-1/src/App.vue
+++ vue3-1/src/App.vue
@@ -14,27 +14,19 @@
       </button>
       <div>你选择的服务员是: {{ selectGirl }}</div>
     </div>
+    <div>
+      <button @click="overAction">点餐完毕</button>
+    </div>
+    <div>
+      {{ overText }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 /* eslint-disable */
 // reactive
-import {
-  reactive,
-  toRefs,
-  onBeforeMount,
-  onMounted,
-  onBeforeUpdate,
-  onUpdated,
-  onBeforeUnmount,
-  onUnmounted,
-  onActivated,
-  onDeactivated,
-  onErrorCaptured,
-  onRenderTracked,
-  onRenderTriggered,
-} from "vue";
+import { reactive, toRefs, ref, watch } from "vue";
 interface DataProps {
   girls: string[];
   selectGirl: string;
@@ -53,49 +45,17 @@ export default {
       },
     });
     const refData = toRefs(data);
-
-    onBeforeMount(() => {
-      console.log("2-组件挂载到页面之前执行 ---- > onBeforeMount()");
-    });
-    onMounted(() => {
-      console.log("3-组件挂载到页面之后执行 ---- > onMounted()");
-    });
-    onBeforeUpdate(() => {
-      console.log("4-组件更新之前执行 ---- > onBeforeUpdate()");
-    });
-    onUpdated(() => {
-      console.log("5-组件更新之后执行 ---- > onUpdated()");
-    });
-    onBeforeUnmount(() => {
-      console.log("6-组件卸载之前执行 ---- > onBeforeUnmount()");
-    });
-    onUnmounted(() => {
-      console.log("7-组件卸载之后执行 ---- > onUnmounted()");
-    });
-    onActivated(() => {
-      console.log("8-组件被激活时执行执 ---- > onActivated()");
-    });
-    // <keep-alive>中多出的钩子函数
-    onDeactivated(() => {
-      console.log(
-        "9-比如从 A 组件，切换到 B 组件，A 组件消失时执行 ---- > onDeactivated()"
-      );
-    });
-    // <keep-alive>中多出的钩子函数
-    onErrorCaptured(() => {
-      console.log(
-        "10-当捕获一个来自子孙组件的异常时激活钩子函数 ---- > onErrorCaptured()"
-      );
-    });
-    // onRenderTracked 状态跟踪 onRenderTriggered 状态触发
-    onRenderTracked((event) => {
-      console.log(event, "11-状态跟踪钩子函数 ---- > onRenderTracked()");
-    });
-    onRenderTriggered((event) => {
-      console.log(event, "12-状态触发钩子函数 ---- > onRenderTriggered()");
+    const overText = ref("晓晓伟餐厅");
+    const overAction = () => {
+      overText.value = "点餐完成 |" + overText.value;
+    };
+    watch([overText,() => data.selectGirl], (newValue,oldValue) => {
+      document.title = newValue[0]
     });
     return {
       ...refData,
+      overText,
+      overAction,
     };
   },
 };
