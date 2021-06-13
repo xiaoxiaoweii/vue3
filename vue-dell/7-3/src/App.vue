5b42018ebc4eba470832940ad6674d99a0424624
xiaoxiaoweii <2679656468@qq.com>
2021-06-13 23:50:15 +0800

    单页面组件实现todolist

---
+++ vue-dell/7-3/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input v-model="inputValue" />
+    <button class="button" @click="handleAddItem">提交</button>
+  </div>
+  <ul>
+    <list-item v-for="(item, index) in list" :key="index" :msg = 'item' />
+  </ul>
+</template>
+
+<script>
+import { reactive,ref } from "vue";
+import listItem from "./components/ListItem.vue";
+export default { 
+  name: "App",
+  components: {
+    listItem
+  },
+  setup() {
+    const list = reactive([]);
+    const inputValue = ref('')
+    const handleAddItem = () => {
+      list.push(inputValue.value)
+      inputValue.value = ''
+    }
+    return {
+      list,
+      inputValue,
+      handleAddItem
+    };
+  },
+};
+</script>
+<style scoped>
+  .button {
+    margin-left: 20px;
+  }
+</style>
---
+++ vue-dell/7-3/src/components/ListItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <li class="button">{{msg}}</li>
+</template>
+<script>
+export default {
+    name: 'ListItem',
+    props: {
+        msg: String
+    },
+    setup() {
+        
+    },
+}
+</script>
