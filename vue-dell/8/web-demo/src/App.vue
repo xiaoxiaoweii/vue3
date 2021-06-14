60a0ce83660db537e336984ed2a463bd7bf920a4
xiaoxiaoweii <2679656468@qq.com>
2021-06-14 23:48:03 +0800

    地区区域布局

--- vue-dell/8/web-demo/src/App.vue
+++ vue-dell/8/web-demo/src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe61f;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="iconfont position_notice">&#xe60a;</span>
+    </div>
+  </div>
   <div class="docker">
     <span class="docker__item docker__item--active">
       <div class="iconfont">&#xe655;</div>
@@ -19,6 +26,34 @@
   </div>
 </template>
 <style lang='scss'>
+@import './style/varibles.scss';
+@import './style/mixins.scss';
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0.5rem;
+  right: 0;
+  padding: 0 0.18rem;
+  .position {
+    position: relative;
+    padding: 0.16rem .24rem .16rem 0;
+    line-height: 0.22rem;
+    font-size: 0.16rem;
+    color: $content-font-color;
+    @include ellipsis;
+    &__icon {
+      margin-right: 0.08rem;
+      font-size: 0.2rem;
+    }
+    .position_notice {
+      position: absolute;
+      right: 0;
+      top: 0.17rem;
+      font-size: .2rem;
+    }
+  }
+}
 .docker {
   display: flex;
   box-sizing: border-box;
@@ -27,7 +62,8 @@
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  border-top: 1px solid #f1f1f1;
+  border-top: 0.01rem solid #f1f1f1;
+  color: $content-font-color;
   &__item {
     flex: 1;
     text-align: center;
