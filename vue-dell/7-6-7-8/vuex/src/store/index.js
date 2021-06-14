c576c49cb4a287519bafc2c9592c254e873d63a1
xiaoxiaoweii <2679656468@qq.com>
2021-06-14 08:38:10 +0800

    composition api vuex

--- vue-dell/7-6-7-8/vuex/src/store/index.js
+++ vue-dell/7-6-7-8/vuex/src/store/index.js
@@ -5,18 +5,25 @@ import {
 // vuex创建了一个全局唯一的仓库 用来存放全局的数据
 export default createStore({
   state: {
-    name: 'xiaoxiaowei'
+    name: 'xiaoxiaowei',
+    age: '18'
   },
   mutations: {
     change(store,value) {
       setTimeout(() => {
         store.name = value
       }, 1000)
+    },
+    changeAge(store,value) {
+      store.age = value
     }
   },
   actions: {
     change(store,value) {
       this.commit('change',value)
+    },
+    changeAge(store,value) {
+      this.commit('changeAge',value)
     }
   },
   modules: {}
--- vue-dell/7-6-7-8/vuex/src/views/Home.vue
+++ vue-dell/7-6-7-8/vuex/src/views/Home.vue
@@ -1,25 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>{{myName}}</h1>
-    <button @click="handleClick">改变</button>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>{{ myName }}</h1>
+    <h1>{{ age }}</h1>
+    <button @click="handleClick">改变名字</button>
+    <button @click="handleClickAge">改变年龄</button>
   </div>
 </template>
 
 <script>
-
+import { toRefs } from "vue";
+import { useStore } from "vuex";
 export default {
-  name: 'Home',
+  name: "Home",
+  setup() {
+    const store = useStore();
+    const { age } = toRefs(store.state);
+    const handleClickAge = () => {
+      store.commit('changeAge',16)
+    }
+    return {
+      age,
+      handleClickAge
+    };
+  },
   computed: {
     myName() {
-      return this.$store.state.name
-    }
+      return this.$store.state.name;
+    },
   },
   methods: {
     handleClick() {
       // 想改变数据 vuex要求第一步 必须派发一个action
-      this.$store.dispatch('change','wei')
-    }
-  }
-}
+      this.$store.dispatch("change", "wei");
+    },
+  },
+};
 </script>
