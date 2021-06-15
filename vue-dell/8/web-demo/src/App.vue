f3504d2cfff73e9005f0cc57c9c9f27557e441a0
xiaoxiaoweii <2679656468@qq.com>
2021-06-15 22:04:54 +0800

    图标列表布局

--- vue-dell/8/web-demo/src/App.vue
+++ vue-dell/8/web-demo/src/App.vue
@@ -12,6 +12,49 @@
     <div class="banner">
       <img class="banner__img" src="@/assets/banner.jpg" />
     </div>
+    <div class="icons">
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img class="icons__item__img" src="@/assets/icon.png" alt="" />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+    </div>
+    <div class="gap"></div>
   </div>
   <div class="docker">
     <span class="docker__item docker__item--active">
@@ -88,7 +131,32 @@
       width: 100%;
     }
   }
+  .icons {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 0.16rem;
+    &__item {
+      width: 20%;
+      &__img {
+        display: block;
+        width: 0.4rem;
+        height: 0.4rem;
+        margin: 0 auto;
+      }
+      &__desc {
+        margin: 0.06rem 0 0.16rem 0;
+        text-align: center;
+        color: $content-font-color;
+      }
+    }
+  }
+  .gap {
+    height: 0.1rem;
+    background: $content-bgColor;
+    margin: 0 -0.18rem;
+  }
 }
+
 .docker {
   display: flex;
   box-sizing: border-box;
@@ -97,7 +165,7 @@
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  border-top: 0.01rem solid #f1f1f1;
+  border-top: 0.01rem solid $content-bgColor;
   color: $content-font-color;
   &__item {
     flex: 1;
