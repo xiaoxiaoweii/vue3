a12ee7c63289667b7059c08e3b4868ed08cef2d0
xiaoxiaoweii <2679656468@qq.com>
2021-06-15 21:46:41 +0800

    搜索及banner

--- vue-dell/8/web-demo/src/App.vue
+++ vue-dell/8/web-demo/src/App.vue
@@ -5,6 +5,13 @@
       北京理工大学国防科技园2号楼10层
       <span class="iconfont position_notice">&#xe60a;</span>
     </div>
+    <div class="search">
+      <span class="iconfont">&#xe706;</span>
+      <span class="search__text">山姆会员商店优惠商品</span>
+    </div>
+    <div class="banner">
+      <img class="banner__img" src="@/assets/banner.jpg" />
+    </div>
   </div>
   <div class="docker">
     <span class="docker__item docker__item--active">
@@ -26,8 +33,8 @@
   </div>
 </template>
 <style lang='scss'>
-@import './style/varibles.scss';
-@import './style/mixins.scss';
+@import "./style/varibles.scss";
+@import "./style/mixins.scss";
 .wrapper {
   position: absolute;
   left: 0;
@@ -37,7 +44,7 @@
   padding: 0 0.18rem;
   .position {
     position: relative;
-    padding: 0.16rem .24rem .16rem 0;
+    padding: 0.16rem 0.24rem 0.16rem 0;
     line-height: 0.22rem;
     font-size: 0.16rem;
     color: $content-font-color;
@@ -50,7 +57,35 @@
       position: absolute;
       right: 0;
       top: 0.17rem;
-      font-size: .2rem;
+      font-size: 0.2rem;
+    }
+  }
+  .search {
+    margin-bottom: 0.12rem;
+    line-height: 0.32rem;
+    background: #f5f5f5;
+    color: #b7b7b7;
+    border-radius: 0.16rem;
+    font-size: 0.14rem;
+    .iconfont {
+      font-size: 0.2rem;
+      display: inline-block;
+      position: relative;
+      top: 0.04rem;
+      padding: 0 0.12rem 0 0.16rem;
+    }
+    &__text {
+      display: inline-block;
+      font-size: 0.14rem;
+    }
+  }
+  .banner {
+    height: 0;
+    overflow: hidden;
+    /* 图片没加载之前 先撑开位置 */
+    padding-bottom: 25.4%;
+    &__img {
+      width: 100%;
     }
   }
 }
